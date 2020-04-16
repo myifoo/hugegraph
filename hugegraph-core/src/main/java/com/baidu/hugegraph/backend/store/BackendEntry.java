e56c1bb536dc98a878a35d233f5334d20b2619db
zhangyi51 <zhangyi51@baidu.com>
2020-04-16 19:02:35 +0800

    add ttl filed in BackendEntry for Cassandra and Hbase
    Change-Id: Ia00f4811b27048629497d476a25dfc7c5c60470b

--- hugegraph-core/src/main/java/com/baidu/hugegraph/backend/store/BackendEntry.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/backend/store/BackendEntry.java
@@ -77,6 +77,8 @@ public interface BackendEntry extends Idfiable {
 
     public Id subId();
 
+    public long ttl();
+
     public int columnsSize();
     public Collection<BackendColumn> columns();
 
